ba9433cd6da58e736d71c6e7347b497dfa550f52
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-04 10:20:49 -0300

    Testando criação de tabuleiro de xadrez com peças brancas e pretas para modelo futuro

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,17 +8,45 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            /*Posicao P;
+            /*Posicao P; // Testando a criação de uma posição
             P = new Posicao(3, 4);
 
             Console.WriteLine("Posição: " + P);
             Console.WriteLine();*/
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tabuleiro tab = new Tabuleiro(8, 8); // Criando um tabuleiro 8x8
 
-            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+            // Modelo Xadrez Padrão
+            for (int i = 0; i < 8; i++)
+            {
+                tab.ColocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, i));
+                tab.ColocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, i));
+                if (i == 0 || i == 7)
+                {
+                    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, i));
+                    tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(7, i));
+                }
+                else if(i == 1 || i == 6)
+                {
+                    tab.ColocarPeca(new Cavalo(Cor.Preta, tab), new Posicao(0, i));
+                    tab.ColocarPeca(new Cavalo(Cor.Branca, tab), new Posicao(7, i));
+                }
+                else if(i == 2 || i == 5)
+                {
+                    tab.ColocarPeca(new Bispo(Cor.Preta, tab), new Posicao(0, i));
+                    tab.ColocarPeca(new Bispo(Cor.Branca, tab), new Posicao(7, i));
+                }
+                else if(i == 3)
+                {
+                    tab.ColocarPeca(new Dama(Cor.Preta, tab), new Posicao(0, i));
+                    tab.ColocarPeca(new Dama(Cor.Branca, tab), new Posicao(7, i));
+                }
+                else
+                {
+                    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, i));
+                    tab.ColocarPeca(new Rei(Cor.Branca, tab), new Posicao(7, i));
+                }
+            }
 
 
             Tela.imprimirTabuleiro(tab);
